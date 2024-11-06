8ff10db2c9a3f249a3ea7233e32e43978ef4cc30
Sergio <sergio.garcia.de.la.cruz@cern.ch>
2024-11-06 16:09:04 +0100

    Install alibuild

--- alma9-alibuild/provision.sh
+++ alma9-alibuild/provision.sh
@@ -1,8 +1,19 @@
 #!/bin/sh -ex
 
-dnf install -y epel-release dnf-plugins-core
-dnf config-manager --set-enabled crb
-
 dnf update -y
-dnf groups install -y 'Development Tools'
-dnf install -y vim git python3 python3-pip python3-devel python3-virtualenv
+dnf install -y python3 python3-pip python3-devel python3-virtualenv
+
+pip install alibuild
+touch ~/.config/alibuild/disable-analytics
+
+# START https://alice-doc.github.io/alice-analysis-tutorial/building/prereq-alma9.html
+cat << EOF > /etc/yum.repos.d/alice-system-deps.repo
+[alice-system-deps]
+name=alice-system-deps
+baseurl=https://s3.cern.ch/swift/v1/alibuild-repo/RPMS/o2-full-deps_el9.x86-64/
+enabled=1
+gpgcheck=0
+EOF
+dnf update -y
+dnf install -y alice-o2-full-deps # alibuild
+# END https://alice-doc.github.io/alice-analysis-tutorial/building/prereq-alma9.html
