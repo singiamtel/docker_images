85e37ed1a5665094fc2e7559c90020bb663ff753
Sergio <sergio.garcia.de.la.cruz@cern.ch>
2024-11-18 19:46:11 +0100

    Add QOL changes to base container

--- alma9_fat/provision.sh
+++ alma9_fat/provision.sh
@@ -5,4 +5,17 @@ dnf config-manager --set-enabled crb
 
 dnf update -y
 dnf groups install -y 'Development Tools'
-dnf install -y vim git
+dnf install -y git neovim python3-neovim
+
+# START Docker
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+dnf install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+# END Docker
+
+{ echo "alias vi='nvim'";
+  echo "alias vim='nvim'";
+  echo "alias e='nvim'";
+} >> /root/.bashrc
+
+
+cat git_alias.sh >> /root/.bashrc # ZSH aliases
